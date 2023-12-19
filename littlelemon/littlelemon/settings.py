dd2e57133696ac22a554960e6e5f2f1f83522487
Ahmed <aakhan2022@gmail.com>
2023-12-20 01:35:49 +0500

    djoser setup

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -135,3 +135,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={"USER_ID_FIELD":"username"}
