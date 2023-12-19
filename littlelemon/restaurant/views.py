c54cb58199d07817bd26c0c21a60ab2e0269a251
Ahmed <aakhan2022@gmail.com>
2023-12-20 02:56:51 +0500

    Securing Bookings Viewset

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -20,3 +21,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
