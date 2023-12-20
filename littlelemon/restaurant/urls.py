0ac9d263aabf57637852f27a5f16baec5d398935
Ahmed <aakhan2022@gmail.com>
2023-12-20 17:29:53 +0500

    added name routing to view

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('menu-items/', views.MenuItemView.as_view()),
-    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemView.as_view(), name='all-menu-items'),
+    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
 ]
