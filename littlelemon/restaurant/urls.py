705dfe4b205ec17c9994295b4721911e1eeea161
Ahmed <aakhan2022@gmail.com>
2023-12-20 18:40:26 +0500

    added obtain auth token in app urls.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('menu-items/', views.MenuItemView.as_view(), name='all-menu-items'),
     path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
+    path('api-token-auth/', obtain_auth_token),
 ]
